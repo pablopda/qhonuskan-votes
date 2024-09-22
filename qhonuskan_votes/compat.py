f21db5f1905c7b0cc62030252aabb327faad0e8c
Pablo Perez De Angelis <pablo@pablopda.com.ar>
2024-09-21 21:29:50 -0300

    refactor: remove compat.py file
    BREAKING CHANGE: Delete compatibility layer for older Django versions
    
    - Remove qhonuskan_votes/compat.py file
    - Project now requires Django 4.2+ as per CHANGES.txt
    - Users should update imports in affected files to use standard Django imports

--- qhonuskan_votes/compat.py
+++
@@ -1,9 +0,0 @@
-from django import VERSION as DJANGO_VERSION
-
-if DJANGO_VERSION >= (2, 0):
-    from django.urls import *
-else:
-    from django.conf.urls import url as re_path, include
-    from django.contrib.auth import get_user_model
-
-    User = get_user_model()
